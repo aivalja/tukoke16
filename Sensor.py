e690e13da15fa8ea7d2f9f65d6d5bd3b6a33ef80
Maiju <anssi.tenhunen@gmail.com>
2016-01-13 13:54:03 +0200

    It now succesfully draws objects

--- Sensor.py
+++ Sensor.py
@@ -5,38 +5,49 @@ Created on Tue Jan 12 09:28:59 2016
 @author: Anssi
 """
 
-import random, time, itertools, numpy as np, matplotlib.pyplot as plt, sys
+import random, time, itertools, numpy as np, matplotlib.pyplot as plt, sys, pygame
 
-x_res = 120
-y_res = 56
-things = sys.argv[0]
+x_res = 1200
+y_res = 560
+things = 10 #int(sys.argv[1])
+background_colour = (255,255,255)
+(width, height) = (1200, 560)
 
 map = [[[{'r':0,'d':0} for k in xrange(9)] for j in xrange(y_res)] for i in xrange(x_res)]
 
 print 'map:' , len(map) , '*' , len(map[0]) , '*' , len(map[0][0])
 
 def add_thing(x,y,layer,width,height,distance,reflectivity):
+    print distance
     for i in range(width):
-        print i
         for j in range(height):
-            #try:
-            print x,y,i,j,layer
-            if map[x+i][y+j][layer]['d'] > distance:
-                map[x+i][y+j][layer]['d'] = distance
-                map[x+i][y+j][layer]['r'] = reflectivity
-            """except:
+            try:
+                if map[x+i][y+j][layer]['d'] < distance:
+                    map[x+i][y+j][layer]['d'] = distance
+                    map[x+i][y+j][layer]['r'] = reflectivity
+            except:
                 #print "Out of range"
-                pass"""
+                pass
 def plot():
     for i in xrange(len(map)):
-        print i
         for j in xrange(len(map[i])):
-            plt.scatter(i,j,s=map[i][j][0]['d'])
+            if (map[i][j][0]['d']!= 0):
+                pygame.draw.rect( screen, (0, 0, (255*map[i][j][0]['d']/100) ), ( (int(i*width/x_res), int(j*height/y_res), int(map[i][j][0]['d']/1), int(map[i][j][0]['d']/1) ) ) )
+                pygame.display.flip()
             #for k in xrange(9):
             #    plt.scatter(i,j,s=map[i][j][k]['d'])
-    plt.grid(True)
-    plt.show()
+print things
+for i in range(things):
+    add_thing(np.random.randint(0,x_res),np.random.randint(0,y_res),0,np.random.randint(1,100),np.random.randint(1,20),np.random.random()*100,np.random.random())
 
-for i in things:
-    add_thing(np.random.randint(0,*x_res,np.random.randint()*y_res,0,np.random.randint(1,100),np.random.randint(1,20),np.random.random()*120,np.random.random())
+screen = pygame.display.set_mode((width, height))
+pygame.display.set_caption('View')
+screen.fill(background_colour)
+
 plot()
+
+running = True
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
