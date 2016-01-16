54d88b3497cce46119ff98eab5b1580ba6a52b0e
Maiju <anssi.tenhunen@gmail.com>
2016-01-16 21:53:40 +0200

    Snow probability implemented succesfully

--- Sensor.py
+++ Sensor.py
@@ -5,31 +5,58 @@ Created on Tue Jan 12 09:28:59 2016
 @author: Anssi
 """
 
-import random, time, itertools, numpy as np, matplotlib.pyplot as plt, sys, pygame
+import random, time, itertools, numpy as np, matplotlib.pyplot as plt, sys, pygame, math
+
+"""
+Equation values
+"""
+snow_to_water = 0.1
 
 x_res = 402
 y_res = 192
 things = 10 #int(sys.argv[1])
+max_distance = 100.0
+
 background_colour = (0,0,0)
 (width, height) = (x_res, int(y_res*2))
 pygame.init()
 clock = pygame.time.Clock()
+# How many snowflakes in cubic meter
+snow_a = 1000
+# Diameter of snowflake in m
+snow_s = 0.01
+# probability of colliding with a snowflake per meter
+snow_p = math.pi*(snow_s/2)**2*snow_a
+
 """
 Create map that includes the "real image"
 """
 map = [[{'r':0,'d':0} for j in xrange(y_res)] for i in xrange(x_res)]
 
 print 'map:' , len(map) , '*' , len(map[0])
+
+"""
+Calculate correct H using some formula
+"""
+def H(R):
+    return R
 """
 Will be used to calculate the strength of return signal
 """
 def pulse_s(x, y):
     point = map[x][y]
+    if point['d'] == 0:
+        x = int(max_distance)
+    else:
+        x = point['d']
+    for i in range(x):
+        if (happens(snow_p)):
+            return (1,i)
     # calculate strength of return signal (0-1)
     if (point['d']!=0 and point['r']!=0):
-        return 1
+        return (1,point['d'])
     else:
-        return 0
+        return (0, max_distance)
 """
 Adds an thing to specific place
 """
@@ -47,25 +74,34 @@ def add_thing(x,y,width,height,distance,reflectivity):
 Draws real image under, where darker = further. Top one is what lidar sees, darker = less likely true
 """
 def plot():
+    screen.fill(background_colour)
     pygame.draw.line(screen,(100,100,100),(0,y_res),(x_res,y_res))
     for i in xrange(x_res):
         for j in xrange(y_res):
             if (map[i][j]['d']!= 0):
-                screen.set_at((int(i), int(j+y_res+1)),((255*map[i][j]['d']/100), (255*map[i][j]['d']/100), (255*map[i][j]['d']/100) ))
+                screen.set_at((int(i), int(j+y_res+1)),(int(255*(1-map[i][j]['d']/max_distance)), int(255*(1-map[i][j]['d']/max_distance)), int(255*(1-map[i][j]['d']/max_distance)) ))
                 #pygame.display.update()
                 
-    for i in range(x_res/3):
+    for i in range(int(x_res/3)):
         #print i
-        for j in range(y_res/3):
-            a = []
+        for j in range(int(y_res/3)):
+            #p = []
+            d = []
             for k in range(3):
                 for l in range(3):
-                    a.append(pulse_s(i*3+k,j*3+l))
-            mean = np.mean(a)
-            screen.fill((255*mean, 255*mean, 255*mean ),rect=((int(i*3), int(j*3),3,3)))
+                    r = pulse_s(i*3+k,j*3+l)
+                    #p.append(r[0])
+                    d.append(r[1])
+            #mean_p = np.mean(p)
+            mean_d = np.mean(d)
+            multiplier = 1-1.0*mean_d/max_distance
+            screen.fill((int(multiplier*255), int(multiplier*255), 0),rect=((int(i*3), int(j*3),3,3)))
             #screen.set_at((int(i), int(j)),(255*mean, 255*mean, 255*mean ))
             #pygame.display.update()
-         
+
+def happens(prob):
+    return random.random()<prob
+
 print things
 for i in range(things):
     add_thing(np.random.randint(0,x_res),np.random.randint(0,y_res),np.random.randint(1,100),np.random.randint(1,20),np.random.randint(0,100),np.random.random())
@@ -80,8 +116,22 @@ running = True
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
+            pygame.quit()
             running = False
-    pygame.display.update()    
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_UP:
+                snow_a+=100
+            if event.key == pygame.K_DOWN:
+                if (snow_a>100):
+                    snow_a-=100
+            if event.key == pygame.K_RIGHT:
+                pass
+            if event.key == pygame.K_LEFT:
+                pass
+            snow_p = math.pi*(snow_s/2)**2*snow_a
+            print snow_a,snow_p
+            plot()
+    pygame.display.update()
     clock.tick(60)
     
     
